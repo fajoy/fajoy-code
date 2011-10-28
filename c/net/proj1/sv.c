237c59664bb8f0803aa7b9afc9001628e5d37438
fajoy <wuminfajoy@gmail.com>
2011-10-29 03:08:12 +0800

    update

--- c/net/proj1/sv.c
+++ c/net/proj1/sv.c
@@ -48,30 +48,34 @@ typedef struct PipeFD{
 }PipeFD;
 
 PipeFD newPipeFD(){
-   PipeFD fd;
-   pipe((int *)&fd);
-   return fd;
+   int fd[2];
+   pipe(fd);
+   PipeFD pfd={fd[0],fd[1]};
+   return pfd;
 }
-void closePipe(PipeFD fd){
-   close(fd.in_fd);
-   close(fd.out_fd);
+void closePipe(PipeFD *fd){
+   close(fd->in_fd);
+   close(fd->out_fd);
+   fd->in_fd=-1;
+   fd->out_fd=-1;
 }
-void closeOutput(PipeFD fd){
-   close(fd.out_fd);
+void closeOutput(PipeFD *fd){
+   close(fd->out_fd);
 }
-void closeInput(PipeFD fd){
-   close(fd.in_fd);
+void closeInput(PipeFD *fd){
+   close(fd->in_fd);
+}
+int isNull(PipeFD *fd)
+{
+   return (fd->in_fd==-1 &&fd->out_fd==-1);
 }
 typedef struct Cmd{
    int p_id;
    int argc;
    char **argv;
-   int pipe[2];
-   int dstpipe[2];
-   int qi;
-   int dst;
    int readFd;
    int writeFd;
+   PipeFD outPipe;
    struct Cmd *parent;
    struct Cmd *first;
    struct Cmd *prev;
@@ -90,11 +94,13 @@ Cmd getCmd(char *cmd,char *del ){
      c.argc =ToArray(arr,&c.argv);
   else
      c.argc=0;
-  c.pipe[0]=-1;
-  c.pipe[1]=-1;
   c.parent=NULL;
   c.prev=NULL;
   c.next=NULL;
+  c.outPipe.in_fd=-1;
+  c.outPipe.out_fd=-1;
+  c.writeFd=-1;
+  c.readFd=-1;
   free(arr);
   return c;
 }
@@ -107,71 +113,52 @@ int spawn(Cmd *cmd)
 
    pid_t child=0;
 
-   dup2Cmd(cmd);
+   //dup2Cmd(cmd);
+   setCmd(cmd);
    child = fork();
 
    if (child != 0) {
       if(child>0){
-         if(cmd->prev!=NULL){
-            if(cmd->pipe[1]==erro_fd){
-               erro_fd=2;
-            }
-            close(cmd->prev->pipe[0]);
-            close(cmd->prev->pipe[1]);
-
-         }
-
-      close(cmd->pipe[1]);
-      cmd->p_id=child;
+         cmd->p_id=child;
       }
       return child;
    } else {
 
 #ifdef debug
-   dprintf(stdo_fd,"(%d)%s  i%d,o%d ",getpid(),cmd->argv[0],cmd->pipe[0],cmd->pipe[1]);
-   dprintf(stdo_fd,"%d-%d %d-%d\n",cmd->dstpipe[0],0,cmd->dstpipe[1],1);
+   dprintf(stdo_fd,"(%d)%s ",getpid(),cmd->argv[0]);
+   if(!isNull(&cmd->outPipe))
+      dprintf(stdo_fd,"i(%d) o(%d) ",cmd->outPipe.in_fd,cmd->outPipe.out_fd);
+   dprintf(stdo_fd,"%d-%d %d-%d\n",cmd->readFd,0,cmd->writeFd,1);
 #endif
       close(0);
       close(1);
-      dup(cmd->dstpipe[0]);
-      dup(cmd->dstpipe[1]);
-
-      close(cmd->dstpipe[0]);
-      close(cmd->dstpipe[1]);
-     if(cmd->prev!=NULL){
-        close(cmd->prev->pipe[1]);
-        close(cmd->prev->pipe[0]);
-     }
-      if(erro_fd==2){
+      dup(cmd->readFd);
+      dup(cmd->writeFd);
+      if(erro_fd==-1){
          close(2);
-         dup(cmd->dstpipe[1]);
-         close(cmd->dstpipe[1]);
+         dup(cmd->parent->outPipe.in_fd);
       }
       else
       {
          close(2);
          dup(erro_fd);
-         close(erro_fd);
       }
-     /* 
+      
       int i=0;
       for(i=1024;i>2;i--){
          close(i);
       }
-      */
       
-      Cmd *next=cmd->first;
-      while(next){
-         close(next->pipe[0]);
-         close(next->pipe[1]);
-         next=next->next;
-      }
+      
+   
+     
+
          execv(strcat(binPATH,prog), arg_list);
          fprintf(stderr,"Unknown command: [%s].\n",prog);
          close(0);
          close(1);
          close(2);
-         return 0;
+         exit(0);
      }
    return child;
 }
@@ -184,30 +171,47 @@ void showCmd(Cmd *cmd){
 
 }
 
-int pipeCmd(Cmd *cmd){
-   pipe(cmd->pipe);
+
+
+
+int setCmd(Cmd *cmd){
    int i,fd=1;
+
+
    for (i=1;i+1<cmd->argc;i++){
       if(strncmp(cmd->argv[i],">",1)==0)
       {
          char * fpath=cmd->argv[i+1];
-         close(cmd->pipe[1]);
          if(strncmp(cmd->argv[i],">>",2)==0)
          fd=open(fpath,O_WRONLY|O_CREAT|O_APPEND,S_IRUSR|S_IWUSR|S_IRGRP|S_IWGRP);
          else
          fd=open(fpath,O_WRONLY|O_CREAT|O_TRUNC,S_IRUSR|S_IWUSR|S_IRGRP|S_IWGRP);
          cmd->argv[i]=NULL;
-         cmd->dst=-1;
+         cmd->writeFd=fd;
       }
    }
+   if(cmd->readFd==-1){
+      if(isNull(&cmd->outPipe)){
+         cmd->readFd=cmd->parent->outPipe.out_fd;
+
+   }
+   else{
+      cmd->readFd=cmd->outPipe.out_fd;
+   }
 }
-void checkPipe(Cmd *cmd){
-   if(cmd->pipe[0]==-1)
-      pipeCmd(cmd);
+if(cmd->writeFd==-1)
+{
+if(cmd->next==NULL)
+   cmd->writeFd=cmd->parent->outPipe.in_fd;
+else{
+   if(isNull(&cmd->next->outPipe))
+   cmd->next->outPipe=newPipeFD();
+   cmd->writeFd=cmd->next->outPipe.in_fd;
 }
-int ddup2(int src ,int dst){
-   return dup2(src,dst);
 }
+
+}
+
 int indexOf(char *str1,char *str2)  
 {  
     char *p=str1;  
@@ -224,47 +228,9 @@ int indexOf(char *str1,char *str2)
         }  
     return i;  
 } 
-int dup2Cmd(Cmd *cmd){
-   checkPipe(cmd);
-   int li =cmd->pipe[0];
-   int lo =cmd->pipe[1];
-   Cmd *prev,*next;
-
-     if(cmd->prev==NULL)
-        prev=cmd->parent;
-     else 
-        prev=cmd->prev;
-
-
-     if(cmd->dst==-1)
-        next=cmd;
-     else if(cmd->next==NULL)
-        next=cmd->parent;
-     else if(cmd->dst==cmd->qi)
-        next=cmd;
-     else if(cmd->dst==cmd->next->qi)
-        next=cmd;
-     else{
-        Cmd *np=cmd->next;
-        while (np){
-           if(cmd->dst==np->qi)
-              break;
-           np=np->next;
-        }
-        next= np->prev;
-
-     }
-
-   checkPipe(prev);
-   checkPipe(next);
-   int ri=prev->pipe[0];
-   int ro=next->pipe[1];
-   cmd->dstpipe[0]=ri;
-   cmd->dstpipe[1]=ro;
-}
 
 int main(int argc,char *argv[],char *envp[]){
-   printf("%d\n",PIPE_BUF);
+   printf("Pipe buffer size=%d\n",PIPE_BUF);
    printf("Input Server Port:");
    char input[10];
    fgets(input,9,stdin);
@@ -317,7 +283,7 @@ int main(int argc,char *argv[],char *envp[]){
             dup(stdo_fd);
             continue;
          }else{
-            erro_fd=2;
+            erro_fd=-1;
             close(mysocket);
             close(stdi_fd);
             char *remote_ip=inet_ntoa(dest.sin_addr);
@@ -325,9 +291,8 @@ int main(int argc,char *argv[],char *envp[]){
             char msg[255];
             char buffer[255];
 
-            Cmd *cmd_que[queMAX];
-            
-            memset(cmd_que,0,queMAX);
+            PipeFD queFd[queMAX];
+            memset(&queFd,-1,queMAX*sizeof(PipeFD));
             int qi=0;
 
             dprintf(stdo_fd,"(%d)%s open\n",getpid(),remote_ip);
@@ -338,10 +303,12 @@ int main(int argc,char *argv[],char *envp[]){
             printf("Your From %s:%d\n",remote_ip,remote_port);
             fflush(stdout);
             
+            PipeFD console;
+            console.out_fd=dup(consocket);
+            console.in_fd=dup(consocket);
             Cmd *c=(Cmd*)malloc(sizeof(Cmd));
-            c->pipe[0]=dup(consocket);
-            c->pipe[1]=dup(consocket);
             c->p_id=getpid();
+            c->outPipe=console;
             while(1){
                int i,j;
                char buffer[255];
@@ -354,8 +321,6 @@ int main(int argc,char *argv[],char *envp[]){
                   break;
                }
                req=strtok(buffer,"\r\n/");
-               cmd_que[qi++]=NULL;
-               qi%=queMAX;
 
 #ifdef debug
                dprintf(stdo_fd,"(%d+%d)%%%s\n",getpid(),qi,req);
@@ -382,31 +347,24 @@ int main(int argc,char *argv[],char *envp[]){
 
                Array *arr=split(req,"|!");
                c->argc =ToArray(arr,&c->argv);
-               c->qi=qi;
-               fflush(stdout);
-               Cmd *prevCmd=cmd_que[qi];
-               if(prevCmd!=NULL){
-                  c->first=prevCmd->first;
-                  c->next=prevCmd->first;
-               }else{
-                  c->first=NULL;
-                  c->first=NULL;
-               }
-
                Cmd *subCmd=malloc(sizeof(Cmd)*c->argc);
                Cmd *nextCmd=subCmd;
+               Cmd *prevCmd=NULL;
+               c->first=NULL;
                for(i=0;i<c->argc;i++){
                   *nextCmd=getCmd(c->argv[i]," \t");
                   if(nextCmd->argc==0)
                      continue;
-                  nextCmd->qi=qi;
-                  nextCmd->dst=qi;
                   if(c->first==NULL){
                      c->first=nextCmd;
-                  }
-                  if (i==0)
-                  {
                      c->next=nextCmd;
+                     if(!isNull(&queFd[qi])){
+                        nextCmd->outPipe=queFd[qi];
+                        nextCmd->readFd=queFd[qi].out_fd;
+
+                        if(erro_fd==queFd[qi].in_fd)
+                           erro_fd=-1;
+                  }
                   }
 
                   if(i+1==c->argc&&i!=0){//final
@@ -425,66 +383,43 @@ int main(int argc,char *argv[],char *envp[]){
                   nextCmd++;
                }
                if(delay>0){
+               
                   int dstqi=(qi+delay)%queMAX;
                   nextCmd=prevCmd;
-                  if(has_err_pipe>0){
-                        has_err_pipe=-1;
-                        checkPipe(prevCmd);
-                        erro_fd=prevCmd->pipe[1];
+                  if(isNull(&queFd[dstqi]))
+                     queFd[dstqi]=newPipeFD();
+                  prevCmd->writeFd=queFd[dstqi].in_fd;
+                  if(has_err_pipe>0)
+                     erro_fd=queFd[dstqi].in_fd;
+                  
 #ifdef debug
-                        dprintf(stdo_fd,"(%d->%d)error pipe %d\n",getpid(),delay,erro_fd );
+                  dprintf(stdo_fd,"(%d->%d)+%d",getpid(),delay,dstqi );
+                  dprintf(stdo_fd,"i(%d) o(%d)\n",queFd[dstqi].in_fd,queFd[dstqi].out_fd );
 #endif
-                  }
-                  prevCmd=cmd_que[dstqi];
-                  nextCmd->dst=dstqi;
-                  cmd_que[dstqi]=nextCmd;
-
-                  if(prevCmd){
-                     prevCmd->next=c->next;
-                     c->next->prev=prevCmd;
-                     nextCmd=prevCmd->next;
-                     while(nextCmd){
-                        nextCmd->first=prevCmd->first;
-                        nextCmd=nextCmd->next;
-                     }
-                  }
-#ifdef debug
-                  dprintf(stdo_fd,"(%d->%d)+%d\n",getpid(),delay,dstqi );
-#endif
-                  continue;
                }
 
 
 
-               nextCmd=c->first;  
+               int status;
                int child_pid;
+               nextCmd=c->first;  
                while(nextCmd){
+                  if(nextCmd->outPipe.in_fd!=console.in_fd)
+                     closeInput(&nextCmd->outPipe);
                   child_pid=spawn(nextCmd);
+
                   if(child_pid==0) //child exec error
                   {
-                     return 0;
+                     exit(0);
                   }else if(child_pid<0){//fork error
                      printf("Shell Error may fork too much.\n");
                      break;
                      //continue;
                   }
-                  nextCmd=nextCmd->next;
-               }
 
-#ifdef debug
-               dprintf(stdo_fd," \n");
-#endif
-               int status;
-               nextCmd=c->first;  
-               while(nextCmd){
-                  close(nextCmd->pipe[0]);
-                  close(nextCmd->pipe[1]);
-                  //f(!kill(nextCmd->p_id,0)){
-                  //
                   do{
 
-                     if( waitpid(nextCmd->p_id, &status, WUNTRACED | WCONTINUED
-                          )==-1)
+                     if( waitpid(nextCmd->p_id, &status, WUNTRACED | WCONTINUED)==-1)
                         break;
                      
 #ifdef debug
@@ -492,21 +427,18 @@ int main(int argc,char *argv[],char *envp[]){
 #endif
                   }while(!WIFEXITED(status) && !WIFSIGNALED(status));
 
-                  //freeCmd(nextCmd);
+                  if(nextCmd->outPipe.out_fd!=console.out_fd)
+                     closeOutput(&nextCmd->outPipe);
                   nextCmd=nextCmd->next;
                }
-              /* 
-               nextCmd=c->first;  
-               while(nextCmd){
-                  prevCmd=nextCmd;
-                  nextCmd=nextCmd->next;
-                  freeCmd(prevCmd);
-               }
-               */
+               free(subCmd);
+               closePipe(&queFd[qi]);
+               qi++;
+               qi%=queMAX;
 
             }
-            close(c->pipe[0]);
-            close(c->pipe[1]);
+          
+            closePipe(&c->outPipe);
             close(consocket);
             close(0);
             close(1);
