d0790c286b30516ed273b9670c1fe1756714dfa0
fajoy <wuminfajoy@gmail.com>
2011-12-22 21:24:37 +0800

    update

--- c/workspace/ras/src/make.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/sh
-mkdir -p ras/bin
-g++ noop.cpp -o ras/bin/noop
-g++ number.cpp -o ras/bin/number
-g++ removetag.cpp -o ras/bin/removetag
-g++ removetag0.cpp -o ras/bin/removetag0
-cp test.html ras
-cp /bin/cat ras/bin
-cp /bin/ls ras/bin
