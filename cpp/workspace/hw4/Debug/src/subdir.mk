e02dd67cf9a9f26252fc3b5fb1696601d72aac69
fajoy <wuminfajoy@gmail.com>
2012-01-05 00:13:05 +0800

    hw4 OK

--- cpp/workspace/hw4/Debug/src/subdir.mk
+++ cpp/workspace/hw4/Debug/src/subdir.mk
@@ -3,6 +3,9 @@
 ################################################################################
 
 # Add inputs and outputs from these tool invocations to the build variables 
+O_SRCS += \
+../src/hw4.o 
+
 CPP_SRCS += \
 ../src/hw4.cpp 
 
